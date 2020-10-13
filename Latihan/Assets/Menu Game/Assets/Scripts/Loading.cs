2f039ffa445649e9727ab62211e028bc2a76ba3e
Muhammad Abbyu J <71117074+mabbyu@users.noreply.github.com>
2020-10-13 10:36:07 +0700

    Add files via upload

---
+++ Latihan/Assets/Menu Game/Assets/Scripts/Loading.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    
+    [SerializeField] private float delayLoading = 3f;
+    [SerializeField] private string namaScene;
+
+    private float timeElapsed;
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > delayLoading)
+        {
+            SceneManager.LoadScene(namaScene);
+        }
+    }
+}
---
+++ Latihan/Assets/Menu Game/Assets/Scripts/Scene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void LoadingScene()
+    {
+        SceneManager.LoadScene("Loading");
+    }
+
+    public void KeluarScene()
+    {
+        Application.Quit();
+    }
+}
